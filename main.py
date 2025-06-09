4e8c1c60389facfd39c3613d5cd8ae83dc54aaf7
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-06-09 14:47:35 +0500

    provide prompt via terminal command

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os 
 from google import genai
 from dotenv import load_dotenv
@@ -6,12 +7,20 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+def prompt_llm():
+    if (len(sys.argv)) < 2: 
+        raise Exception("Usage: python3 main,.py <PROMPT>") 
+    prompt = sys.argv[1]
 
-print(response.text)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=prompt
+    )
 
-print("Prompt tokens:",response.usage_metadata.prompt_token_count)
-print("Response tokens:",response.usage_metadata.candidates_token_count)
+    print(response.text)
+
+    print("Prompt tokens:",response.usage_metadata.prompt_token_count)
+    print("Response tokens:",response.usage_metadata.candidates_token_count)
+
+if __name__ == "__main__":
+    prompt_llm()
