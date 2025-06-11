ffc68a73c6b0192fb2fff0e128b0e1cd086277ef
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-06-12 01:16:50 +0500

    allow agent to execute python code files

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from functions.write_file import write_file
-
+from functions.run_python_file import run_python_file
 
 def test():
     result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
@@ -11,6 +11,18 @@ def test():
     result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
+    
+    result = run_python_file("calculator", "main.py")
+    print(result) 
+
+    result = run_python_file("calculator", "tests.py")
+    print(result) 
+
+    result = run_python_file("calculator", "../main.py")
+    print(result) 
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result) 
 
 if __name__ == "__main__":
     test()
